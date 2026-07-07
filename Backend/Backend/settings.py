e88d496f44469ca7f438dd10788a7c4fce78935e
Ayush Shrestha <ayushsth007@gmail.com>
2026-07-07 19:43:02 +0545

    Configure PostgreSQL database

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -109,10 +109,10 @@ WSGI_APPLICATION = "Backend.wsgi.application"
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
